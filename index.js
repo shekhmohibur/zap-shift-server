954644e288cf567c3e28a700914e1cb66e46bcc9
Shekh Mohibur Rahman <119482264+shekhmohibur@users.noreply.github.com>
2026-03-05 23:52:42 +0600

    simple server setup done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
