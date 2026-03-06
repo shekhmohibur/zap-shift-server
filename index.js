346929dfb8c18d9167c8b466a500fcf0362b7f5e
Shekh Mohibur Rahman <119482264+shekhmohibur@users.noreply.github.com>
2026-03-06 13:00:39 +0600

    updated code with get,post parcel

--- index.js
+++ index.js
@@ -12,6 +12,45 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    //collections are here 
+    const parcelCollection = client.db('parcelDB').collection('parcels');
+
+    //CRUDS are here
+    app.get('/parcels', async(req, res) => {
+      const parcels = await parcelCollection.find().toArray();
+      res.send(parcels)
+    })
+    //adding parcel to db
+    app.post('/parcels', async(req, res) => {
+      try{
+        const newParcel = req.body;
+        const addParcel = await parcelCollection.insertOne(newParcel);
+        res.send(addParcel);
+      }catch(error){
+        console.error('found error while inserting parcel', error)
+        res.status(500).send({message:'failed to create parcel'})
+      }
+    })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
