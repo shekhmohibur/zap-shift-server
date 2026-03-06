925a063334825e1d1f287f50dfa244fdb9b07de7
Shekh Mohibur Rahman <119482264+shekhmohibur@users.noreply.github.com>
2026-03-06 23:00:33 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.DB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,32 +20,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
     await client.connect();
-    //collections are here 
-    const parcelCollection = client.db('parcelDB').collection('parcels');
+    //collections are here
+    const parcelCollection = client.db("parcelDB").collection("parcels");
 
     //CRUDS are here
-    app.get('/parcels', async(req, res) => {
-      const parcels = await parcelCollection.find().toArray();
-      res.send(parcels)
-    })
+    app.get("/parcels", async (req, res) => {
+      const email = req.query.email;
+      const query = email ? { created_by: email } : {};
+      const options = {
+        sort: { createdAt: -1 },
+      };
+      const parcels = await parcelCollection.find(query, options).toArray();
+      res.send(parcels);
+    });
     //adding parcel to db
-    app.post('/parcels', async(req, res) => {
-      try{
+    app.post("/parcels", async (req, res) => {
+      try {
         const newParcel = req.body;
         const addParcel = await parcelCollection.insertOne(newParcel);
         res.send(addParcel);
-      }catch(error){
-        console.error('found error while inserting parcel', error)
-        res.status(500).send({message:'failed to create parcel'})
+      } catch (error) {
+        console.error("found error while inserting parcel", error);
+        res.status(500).send({ message: "failed to create parcel" });
       }
-    })
+    });
+    //deleting parcel from db
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };   
+      console.log(query);
+      
+      const deleteParcel = await parcelCollection.deleteOne(query);
+      res.send(deleteParcel);   
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
   }
 }
