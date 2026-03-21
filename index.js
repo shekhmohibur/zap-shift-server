51a476fb354f482a8d3a61c8a7bf0dbfa7cdf486
Shekh Mohibur Rahman <119482264+shekhmohibur@users.noreply.github.com>
2026-03-21 14:49:16 +0600

    fixed for vercel

--- index.js
+++ index.js
@@ -456,6 +456,4 @@ app.patch("/parcels", async (req, res) => {
 }
 run().catch(console.dir);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
